4e6f178a5e5475a2ab0b4e79d9895d59be24d35a
Saumitra Shukla <64691756+Saumitra23@users.noreply.github.com>
2022-06-17 10:41:17 +0530

    new pages

---
+++ Nicepage/basic1/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import HeaderComp from "./components/Header/Header";
+import Body from "./components/body/body";
+import Footer from "./components/Footer/Footer";
+const App = () => {
+  return (
+    <div>
+      <HeaderComp />
+      <Body />
+      <Footer />
+    </div>
+  );
+};
+export default App;
---
+++ Nicepage/basic1/src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import "./style.css";
+
+const Footer = () => {
+  return (
+    <div className="test">
+      <div>Headline</div>
+      <div>Headline</div>
+      <div>Headline</div>
+    </div>
+  );
+};
+
+export default Footer;
