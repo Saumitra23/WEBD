fdc322bc2f069d0d1643a1a15f3fb643596ee846
Saumitra Shukla <64691756+Saumitra23@users.noreply.github.com>
2022-06-18 18:32:32 +0530

    header and form added

--- Nicepage/basic2/src/App.js
+++ Nicepage/basic2/src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { slide as Menu } from "react-burger-menu";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1 style={{ color: "RGB(187, 187, 187)" }}>logo</h1>
       </header>
+      <div>
+        <Menu>
+          <a id="home" className="menu-item" href="/">
+            Home
+          </a>
+          <a id="about" className="menu-item" href="/about">
+            About
+          </a>
+          <a id="contact" className="menu-item" href="/contact">
+            Contact
+          </a>
+        </Menu>
+      </div>
+      <div className="main">
+        <div className="el_1">
+          <img
+            src="https://images01.nicepage.com/c461c07a441a5d220e8feb1a/0aaea178def3575188426175/gggg-min.jpg"
+            width={532}
+            height={657}
+          ></img>
+        </div>
+        <div className="el_2">
+          <h1>Contact Form</h1>
+          <form>
+            <input
+              type="text"
+              placeholder="Enter your name"
+              name="name"
+            ></input>
+            <input
+              type="text"
+              placeholder="Enter a valid email address"
+              name="name"
+            ></input>
+            <input
+              type="text"
+              placeholder="Enter your message"
+              name="name"
+            ></input>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
