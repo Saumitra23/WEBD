5f4bd9e689a7e0cf8157d4a9af02e688e2e34539
Saumitra Shukla <64691756+Saumitra23@users.noreply.github.com>
2022-06-19 09:34:27 +0530

    social media links added

--- Nicepage/basic2/src/App.js
+++ Nicepage/basic2/src/App.js
@@ -1,13 +1,11 @@
 import "./App.css";
 import { slide as Menu } from "react-burger-menu";
+import { SocialMediaIconsReact } from "social-media-icons-react";
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <h1 style={{ color: "RGB(187, 187, 187)" }}>logo</h1>
-      </header>
-      <div>
+      <div className="menu">
         <Menu>
           <a id="home" className="menu-item" href="/">
             Home
@@ -20,12 +18,15 @@ function App() {
           </a>
         </Menu>
       </div>
+      <div className="head" style={{ color: "RGB(187, 187, 187)" }}>
+        logo
+      </div>
       <div className="main">
         <div className="el_1">
           <img
             src="https://images01.nicepage.com/c461c07a441a5d220e8feb1a/0aaea178def3575188426175/gggg-min.jpg"
-            width={532}
-            height={657}
+            width={472}
+            height={597}
           ></img>
         </div>
         <div className="el_2">
@@ -41,12 +42,68 @@ function App() {
               placeholder="Enter a valid email address"
               name="name"
             ></input>
-            <input
-              type="text"
-              placeholder="Enter your message"
-              name="name"
-            ></input>
+            <textarea placeholder="Enter your message" name="name"></textarea>
           </form>
+          <button>Submit</button>
+          <h1>Follow us</h1>
+          <div className="icons">
+            <div>
+              <SocialMediaIconsReact
+                borderColor="rgba(0,0,0,0.25)"
+                borderWidth="2"
+                borderStyle="solid"
+                icon="twitter"
+                iconColor="rgba(255,255,255,1)"
+                backgroundColor="rgba(26,166,233,1)"
+                iconSize="5"
+                roundness="20%"
+                url="https://some-website.com/my-social-media-url"
+                size="30"
+              />
+            </div>
+            <div>
+              <SocialMediaIconsReact
+                borderColor="rgba(0,0,0,0.25)"
+                borderWidth="2"
+                borderStyle="solid"
+                icon="facebook"
+                iconColor="rgba(255,255,255,1)"
+                backgroundColor="RGB(24, 119, 242)"
+                iconSize="5"
+                roundness="20%"
+                url="https://some-website.com/my-social-media-url"
+                size="30"
+              />
+            </div>
+            <div>
+              <SocialMediaIconsReact
+                borderColor="rgba(0,0,0,0.25)"
+                borderWidth="2"
+                borderStyle="solid"
+                icon="instagram"
+                iconColor="rgba(255,255,255,1)"
+                backgroundColor="RGB(199, 60, 143)"
+                iconSize="5"
+                roundness="20%"
+                url="https://some-website.com/my-social-media-url"
+                size="30"
+              />
+            </div>
+            <div>
+              <SocialMediaIconsReact
+                borderColor="rgba(0,0,0,0.25)"
+                borderWidth="2"
+                borderStyle="solid"
+                icon="github"
+                iconColor="black"
+                backgroundColor="RGB(205, 217, 229)"
+                iconSize="5"
+                roundness="20%"
+                url="https://some-website.com/my-social-media-url"
+                size="30"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
