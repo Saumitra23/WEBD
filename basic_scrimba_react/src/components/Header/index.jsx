463be9f42120fe8ca9904b03d5972d4d780a67f0
Saumitra Shukla <64691756+Saumitra23@users.noreply.github.com>
2022-06-28 12:48:29 +0530

    page created

---
+++ basic_scrimba_react/src/components/Header/index.jsx
@@ -0,0 +1,18 @@
+import "./style.css";
+
+const Header = () => {
+  return (
+    <div className="head">
+      <div className="el_1">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png"
+          width={30}
+        ></img>
+      </div>
+      <div className="el_2">ReactFacts</div>
+      <div className="el_3">React Course - Project 1</div>
+    </div>
+  );
+};
+
+export default Header;
