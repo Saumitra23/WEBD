6803611f5cb24567129db1429f7e5c37ca433add
Saumitra Shukla <64691756+Saumitra23@users.noreply.github.com>
2022-06-28 12:50:10 +0530

    updated and final code

--- basic_scrimba_react/src/components/Header/index.jsx
+++ basic_scrimba_react/src/components/Header/index.jsx
@@ -7,6 +7,7 @@ const Header = () => {
         <img
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png"
           width={30}
+          alt="oopss"
         ></img>
       </div>
       <div className="el_2">ReactFacts</div>
