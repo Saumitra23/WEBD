f9afc93e685003c623e73eec5ac7927b885bad05
Saumitra Shukla <64691756+Saumitra23@users.noreply.github.com>
2022-06-29 13:41:40 +0530

    minor changes

--- basic_scrimba_react/src/components/Header/index.jsx
+++ basic_scrimba_react/src/components/Header/index.jsx
@@ -3,15 +3,16 @@ import "./style.css";
 const Header = () => {
   return (
     <div className="head">
-      <div className="el_1">
+      <div>
         <img
+          className="el_1"
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png"
           width={30}
           alt="oopss"
         ></img>
       </div>
-      <div className="el_2">ReactFacts</div>
-      <div className="el_3">React Course - Project 1</div>
+      <h3 className="el_2">ReactFacts</h3>
+      <h4 className="el_3">React Course - Project 1</h4>
     </div>
   );
 };
