608fdca5a31afffd520e7bd3ff7b8e87cffa4bb7
saumitra23 <saumitrashukla23@hmail.com>
2022-11-28 17:58:15 +0530

    test commit

--- basic_scrimba_react/src/App.js
+++ basic_scrimba_react/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Header from "./components/Header";
 import Card from "./components/Card";
 const App = () => {
+  console.log("I hope this works!!!!")
   return (
     <div className="container">
       <Header />
